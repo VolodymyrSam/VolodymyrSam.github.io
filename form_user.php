73fd674def3bf018070597df47d0b20764fa202b
Vladimir <vova290594@gmail.com>
2018-10-22 21:13:29 +0200

    rename php file

---
+++ form_user.php
@@ -0,0 +1,10 @@
+<?php
+echo "inform received";
+$name = $_POST['user_name'];
+$email = $_POST['user_email'];
+echo "привет";
+echo $name;
+echo "твоя почта регистрации";
+echo $email;
+
+?>
--- user_name.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "inform received"
-?>
